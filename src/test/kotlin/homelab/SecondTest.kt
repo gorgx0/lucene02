ea5bad5a3197c3dd08c96913b07dc2515d24bea0
Marcin Krasowski <marcin.krasowski@gmail.com>
2024-01-02 14:17:50 +0100

    second test - many results

--- src/test/kotlin/homelab/SecondTest.kt
+++ src/test/kotlin/homelab/SecondTest.kt
@@ -3,6 +3,7 @@ package homelab
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.document.Document
 import org.apache.lucene.document.Field
+import org.apache.lucene.document.StringField
 import org.apache.lucene.document.TextField
 import org.apache.lucene.index.DirectoryReader
 import org.apache.lucene.index.IndexWriter
@@ -15,26 +16,26 @@ import kotlin.test.assertEquals
 
 class SecondTest {
 
-    val offers = TestDataReader().offers("test_data.json")
-    val analyzer = StandardAnalyzer()
-    val dir = ByteBuffersDirectory()
-    val indexWriterConfig = IndexWriterConfig(analyzer)
-    val indexWriter = IndexWriter(dir, indexWriterConfig)
+    private val offers = TestDataReader().offers("test_data.json")
+    private val analyzer = StandardAnalyzer()
+    private val dir = ByteBuffersDirectory()
+    private val indexWriterConfig = IndexWriterConfig(analyzer)
+    private val indexWriter = IndexWriter(dir, indexWriterConfig)
 
     @Test
     fun indexingOffers() {
         offers.forEach {
             val document = Document()
             with(document) {
-                add(Field("property_id", it.propertyId, TextField.TYPE_STORED))
+                add(Field("property_id", it.propertyId, StringField.TYPE_STORED))
                 add(Field("type", it.type, TextField.TYPE_STORED))
                 add(Field("location", it.location, TextField.TYPE_STORED))
                 add(Field("price", it.price, TextField.TYPE_STORED))
                 add(Field("bedrooms", it.bedrooms.toString(), TextField.TYPE_STORED))
                 add(Field("bathrooms", it.bathrooms.toString(), TextField.TYPE_STORED))
                 add(Field("area_sqft", it.areaSqft.toString(), TextField.TYPE_STORED))
-                add(Field("description", it.description, TextField.TYPE_STORED))
-                add(Field("image_url", it.imageUrl, TextField.TYPE_STORED))
+                add(Field("description", it.description, TextField.TYPE_NOT_STORED))
+                add(Field("image_url", it.imageUrl, StringField.TYPE_STORED))
             }
             indexWriter.addDocument(document)
         }
@@ -45,12 +46,19 @@ class SecondTest {
         val queryParser = QueryParser("description", analyzer)
         val query = queryParser.parse("description:renovated")
         val topDocs = searcher.search(query, 10)
-        assertEquals(1L, topDocs.totalHits.value)
-        val storedFields = searcher.storedFields()
-        val first = topDocs.scoreDocs.first()
-        val document = storedFields.document(first.doc)
-        assertEquals("REO002", document.get("property_id"))
+
+        assertEquals(2L, topDocs.totalHits.value)
+
+        val propertyIds = topDocs.scoreDocs.map {
+            searcher.storedFields().document(it.doc).get("property_id")
+        }
+
+        topDocs.scoreDocs.forEach {
+            println("------------------")
+            val fields = searcher.indexReader.document(it.doc).fields
+            fields.forEach(::println)
+        }
+
+        assertEquals(listOf("REO005", "REO002"), propertyIds)
     }
-
-
 }
